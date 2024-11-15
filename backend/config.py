83d9e3167352adf7b3aa2fd82e66e134f51dbceb
Viona <vionasequeir9@gmail.com>
2024-11-15 14:08:06 +0530

    changed

--- backend/config.py
+++ backend/config.py
@@ -3,9 +3,9 @@ from datetime import timedelta
 from urllib.parse import quote_plus
 
 class Config:
-    DB_PASSWORD = 'root'
+    DB_PASSWORD = 'vijju@2004'
     ENCODED_PASSWORD = quote_plus(DB_PASSWORD)
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{ENCODED_PASSWORD}@localhost:3306/waste_management_system"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{ENCODED_PASSWORD}@127.0.0.1:3305/waste_management_system"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your-jwt-secret-key-here'
